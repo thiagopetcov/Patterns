b2f660c4d2e1dab6541c10cc162bde11101b9b3b
thiagopetcov <thiago.petcov@gmail.com>
2020-03-25 21:26:28 +0000

    3commitClass

---
+++ src/Country.java
@@ -0,0 +1,20 @@
+
+public class Country {
+	//Atributes 
+		private String code;
+		private String name;
+		private String continent;
+		private float surfaceArea;
+		private String headOfState;
+		
+		//Object is ready to be made.
+		
+		public Country(String code, String name, String continent, float surfaceArea, String headOfState) {
+			this.code = code;
+			this.name = name;
+			this.continent = continent;
+			this.surfaceArea = surfaceArea;
+			this.headOfState = headOfState;
+		}
+}
+
